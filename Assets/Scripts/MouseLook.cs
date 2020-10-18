e2605b3062814ce9d6ce3b01a442449ee8d47b3d
DESKTOP-T88A28Q\Brand <BrandonJSoares@gmail.com>
2020-10-18 14:29:27 -0400

    Added first-person player movement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Protects from duplicate scripts
+[DisallowMultipleComponent]
+
+public class MouseLook : MonoBehaviour
+{
+    // Initialize variables
+    [SerializeField] float mouseSense = 300f;
+    private float yRotation = 0f;
+    private GameObject playerCharacter;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cursor inprovements
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Find and set player object
+        playerCharacter = GameObject.Find("PlayerCharacter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
+        
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        playerCharacter.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float movementSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * yAxis + transform.right * xAxis;
+        characterController.Move(movementSpeed * Time.deltaTime * move);
+    }
+}
